91eddd4ff75c7ad766e14d2a2d339b60a0264903
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-05-18 19:01:15 +0900

    make Person

--- PrefectureCompatibilityChecker/Models/Person.swift
+++ PrefectureCompatibilityChecker/Models/Person.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct Person: Codable {
+    var name: String
+    var birthday: DateStruct
+    var bloodType: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case birthday
+        case bloodType = "blood_type"
+    }
+}
