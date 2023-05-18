651f50889f6de15885608ff73d2f4631d05f512a
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-05-19 00:09:56 +0900

    make FortuneResult

--- PrefectureCompatibilityChecker/Models/FortuneResult.swift
+++ PrefectureCompatibilityChecker/Models/FortuneResult.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct FortuneResult: Decodable {
+    let prefecture: String
+    let capital: String
+    let citizenDay: MonthDay?
+    let hasCoastLine: Bool
+    private let logoURL: String
+    var prefectureImageURL: URL? {
+        return URL(string: logoURL)
+    }
+    let brief: String
+    
+    struct MonthDay: Decodable {
+        let month: Int
+        let day: Int
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case prefecture = "name"
+        case capital
+        case citizenDay = "citizen_day"
+        case hasCoastLine = "has_coast_line"
+        case logoURL = "logo_url"
+        case brief
+    }
+}
