39dfa42cf763287ddf7d17f5335f0de988456a13
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-05-18 12:13:56 +0900

    Initial commit

---
+++ PrefectureCompatibilityChecker/PrefectureCompatibilityCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrefectureCompatibilityCheckerApp.swift
+//  PrefectureCompatibilityChecker
+//
+//  Created by Eisuke KASAHARA on 2023/05/18.
+//
+
+import SwiftUI
+
+@main
+struct PrefectureCompatibilityCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
