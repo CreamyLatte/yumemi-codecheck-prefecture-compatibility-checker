6404e19deb132f1b795d2da044c93a0b8e64df84
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-05-18 18:39:36 +0900

    add FortuneResult.swift and Person.swift

---
+++ PrefectureCompatibilityChecker/Models/FortuneResult.swift
@@ -0,0 +1,8 @@
+//
+//  FortuneResult.swift
+//  PrefectureCompatibilityChecker
+//
+//  Created by Eisuke KASAHARA on 2023/05/18.
+//
+
+import Foundation
---
+++ PrefectureCompatibilityChecker/Models/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  PrefectureCompatibilityChecker
+//
+//  Created by Eisuke KASAHARA on 2023/05/18.
+//
+
+import Foundation
