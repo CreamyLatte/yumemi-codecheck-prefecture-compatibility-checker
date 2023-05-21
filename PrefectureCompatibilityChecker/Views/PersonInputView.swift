e4a3e145200325bb1c5d47d7dd4e455be02f9911
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-05-21 14:31:34 +0900

    add PersonInputView.swift and ResultView.swift.

---
+++ PrefectureCompatibilityChecker/Views/PersonInputView.swift
@@ -0,0 +1,20 @@
+//
+//  PersonInputView.swift
+//  PrefectureCompatibilityChecker
+//
+//  Created by Eisuke KASAHARA on 2023/05/21.
+//
+
+import SwiftUI
+
+struct PersonInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PersonInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonInputView()
+    }
+}
---
+++ PrefectureCompatibilityChecker/Views/ResultView.swift
@@ -0,0 +1,20 @@
+//
+//  ResultView.swift
+//  PrefectureCompatibilityChecker
+//
+//  Created by Eisuke KASAHARA on 2023/05/21.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultView()
+    }
+}
