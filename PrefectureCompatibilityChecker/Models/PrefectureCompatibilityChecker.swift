65e72739a6c6cab3465e88d3d1173c47e84bdf96
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-05-21 15:12:34 +0900

    add load method.

--- PrefectureCompatibilityChecker/Models/PrefectureCompatibilityChecker.swift
+++ PrefectureCompatibilityChecker/Models/PrefectureCompatibilityChecker.swift
@@ -102,4 +102,25 @@ extension PrefectureCompatibilityChecker {
 let testPerson = Person(name: "John Doe", birthday: Calendar.current.date(from: DateComponents(year: 1999, month: 6, day: 3))!, bloodType: "a")
 let checker = PrefectureCompatibilityChecker()
 
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
 
