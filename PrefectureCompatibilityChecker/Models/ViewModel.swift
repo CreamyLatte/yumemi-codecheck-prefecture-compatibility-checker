764a6ee337c5a84e4578066f493958f7aad17f89
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-05-22 19:15:21 +0900

    make ViewModel

--- PrefectureCompatibilityChecker/Models/ViewModel.swift
+++ PrefectureCompatibilityChecker/Models/ViewModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+@MainActor
+final class ViewModel: ObservableObject {
+    @Published var isInput: Bool = false
+    @Published var isResult: Bool = false
+    
+    let prefectureCompatibilityChecker = PrefectureCompatibilityChecker()
+    func checkCompatibility(person: Person) async -> FortuneResult? {
+        let fortuneResult: FortuneResult
+        do {
+            fortuneResult = try await prefectureCompatibilityChecker.checkCompatibility(person: person)
+        } catch {
+            print("Couldn't tell a fortune. \(error)")
+            return nil
+        }
+        return fortuneResult
+    }
+    
+    func showInputView() {
+        isInput = true
+    }
+    func showResultView() {
+        isResult = true
+    }
+    func showMainView() {
+        isInput = false
+        isResult = false
+    }
+}
