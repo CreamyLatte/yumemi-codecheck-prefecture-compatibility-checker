cf863c62b1743e69e5f54138c90cfe211c95f8be
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-05-24 01:01:02 +0900

    make TitleView.

--- PrefectureCompatibilityChecker/Views/TitleView.swift
+++ PrefectureCompatibilityChecker/Views/TitleView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct TitleView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("japan")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 320)
+                .blur(radius: 2.0)
+            VStack(spacing: 20) {
+                Text("あなたに合う")
+                    .font(.title2)
+                Text("47都道府県\n相性マッチング")
+                    .font(.title)
+            }
+            .bold()
+            .multilineTextAlignment(.center)
+        }
+        .padding()
     }
 }
 
