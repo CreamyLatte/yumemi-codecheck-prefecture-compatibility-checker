d7d49e4c0ea606ce0ff80415636d26d2255be574
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-05-23 10:57:19 +0900

    add TitleView.swift.

---
+++ PrefectureCompatibilityChecker/Views/TitleView.swift
@@ -0,0 +1,20 @@
+//
+//  TitleView.swift
+//  PrefectureCompatibilityChecker
+//
+//  Created by Eisuke KASAHARA on 2023/05/23.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleView()
+    }
+}
