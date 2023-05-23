fc397ea8ba1ce957b0ba5edc5b94ac6286cb4614
CreamyLatte <48711020+CreamyLatte@users.noreply.github.com>
2023-05-24 00:59:10 +0900

    make CapsuleButtonStyle.

--- PrefectureCompatibilityChecker/Views/ButtonStyle/CapsuleButtonStyle.swift
+++ PrefectureCompatibilityChecker/Views/ButtonStyle/CapsuleButtonStyle.swift
@@ -5,4 +5,20 @@
 //  Created by Eisuke KASAHARA on 2023/05/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct CapsuleButtonStyle: ButtonStyle {
+    @Environment(\.isEnabled) var isEnabled: Bool
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.title3)
+            .bold()
+            .frame(maxWidth: 400)
+            .padding()
+            .background((self.isEnabled ? Color.green : Color.gray).opacity(configuration.isPressed ? 0.9 : 1.0))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 0.975 : 1.0)
+    }
+}
